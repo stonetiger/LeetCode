eaf8c1d0aef099c20ebfe808b6787354516afe73
stonetiger <songchaoschip@163.com>
2018-12-03 21:59:52 +0800

    IsSameTree

---
+++ LeetCodeAnswer/src/leetcode/answer/inprocess/IsSameTree.java
@@ -0,0 +1,37 @@
+package leetcode.answer.inprocess;
+
+public class IsSameTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) {
+            return false;
+        }
+        else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        t1.left = new TreeNode(2);
+        t1.right = new TreeNode(3);
+
+        TreeNode t2 = new TreeNode(1);
+        t2.left = new TreeNode(2);
+        t2.right = new TreeNode(3);
+
+        IsSameTree ist = new IsSameTree();
+        System.out.println(ist.isSameTree(t1, t2));
+    }
+}
